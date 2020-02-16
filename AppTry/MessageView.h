4bd2478bef59a10e8848e2bb41717be9229c60ab
NataliaPatiño <54645217+Natalia-SP@users.noreply.github.com>
2020-02-16 10:48:37 -0600

    Create MessageView.h

---
+++ AppTry/MessageView.h
@@ -0,0 +1,13 @@
+//  Created by md on 6/24/17.
+//  Copyright © 2017 ruanestudios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MessageView : UIVisualEffectView
+- (void)queueMessage: (NSString *)message;
+@property (nonatomic, retain) NSString *currentMessage;
+@property (nonatomic, retain) NSString *nextMessage;
+@property (nonatomic, retain) NSTimer *timer;
+@end
